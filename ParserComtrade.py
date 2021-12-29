efc87ea87e9bb0c219a959d0d462ff80cca46362
Roman_Max <maksimow.roma2018@yandex.ru>
2021-12-29 08:59:40 +0300

    нужно скорректировать синхронизацию

--- ParserComtrade.py
+++ ParserComtrade.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class Parser_comtrade():
-    def __init__(self, path="2", flag="224 Пуск осциллогр.", time_d=20000 / 24):
+    def __init__(self, path="2", flag="224 Пуск осциллогр.", time_d=20000 / 24, A=True, D=True):
         self.__path_cfg = path + ".cfg"
         self.__path_dat = path + ".dat"
         self.__data_A = {}
@@ -17,22 +17,30 @@ class Parser_comtrade():
         self.index_end_time = -1
         self.__numbers_analog = 0
         self.__numbers_digital = 0
+        self.numbers_signal = 0
         self.times = list(range(0, 5521667, 683))
         self.matrix_analog = numpy.zeros((1, 1))
         self.matrix_digital = numpy.zeros((1, 1))
         self.index_time = -1
+        self.data_cfg = []
+        self.data_dat = []
+        self.__A = A
+        self.__D = D
 
     def pars_cfg(self):
         with open(self.__path_cfg, 'r') as fp:
-            data = fp.readlines()
-            self.__numbers_analog = int(data[1].split(",")[1].split("A")[0])
-            self.__numbers_digital = int(data[1].split(",")[2].split("D")[0])
+            self.data_cfg = fp.readlines()
+            self.__numbers_analog = int(self.data_cfg[1].split(",")[1].split("A")[0])
+            self.__numbers_digital = int(self.data_cfg[1].split(",")[2].split("D")[0])
             for number_analog in range(self.__numbers_analog):
-                self.name_analog.append(data[2 + number_analog].split(",")[1])
-                self.__data_A[data[2 + number_analog].split(",")[1]] = float(data[2 + number_analog].split(",")[5])
-                self.__data_B[data[2 + number_analog].split(",")[1]] = float(data[2 + number_analog].split(",")[6])
+                self.name_analog.append(self.data_cfg[2 + number_analog].split(",")[1])
+                self.__data_A[self.data_cfg[2 + number_analog].split(",")[1]] = float(
+                    self.data_cfg[2 + number_analog].split(",")[5])
+                self.__data_B[self.data_cfg[2 + number_analog].split(",")[1]] = float(
+                    self.data_cfg[2 + number_analog].split(",")[6])
             for number_digital in range(self.__numbers_analog, self.__numbers_analog + self.__numbers_digital):
-                self.name_digital.append(data[2 + number_digital].split(",")[1])
+                self.name_digital.append(self.data_cfg[2 + number_digital].split(",")[1])
+            self.numbers_signal = self.__numbers_analog + self.__numbers_digital
         # print(self.__data_A)
         # print(self.__data_B)
         # print(self.__name_analog)
@@ -41,25 +49,25 @@ class Parser_comtrade():
 
     def pars_dat(self):
         with open(self.__path_dat, 'r') as fp:
-            data = fp.readlines()
-            self.time_d = int(data[1].split(",")[1])
-            self.end_time = int(data[len(data) - 1].split(",")[1])
-            self.index_end_time = int(data[len(data) - 1].split(",")[0])
-            # self.matrix_analog = numpy.zeros((len(data), self.__numbers_analog + 1))
-            # for t in range(len(data)):
-            #     self.matrix_analog[t, 0] = int(data[t].split(",")[1])
+            self.data_dat = fp.readlines()
+            self.time_d = int(self.data_dat[1].split(",")[1])
+            self.end_time = int(self.data_dat[len(self.data_dat) - 1].split(",")[1])
+            self.index_end_time = int(self.data_dat[len(self.data_dat) - 1].split(",")[0])
+            # self.matrix_analog = numpy.zeros((len(data_dat), self.__numbers_analog + 1))
+            # for t in range(len(data_dat)):
+            #     self.matrix_analog[t, 0] = int(data_dat[t].split(",")[1])
             #     for number_analog in range(self.__numbers_analog):
             #         a = self.__data_A[self.__name_analog[number_analog]]
             #         b = self.__data_B[self.__name_analog[number_analog]]
-            #         x = int(data[t].split(",")[2 + number_analog])
+            #         x = int(data_dat[t].split(",")[2 + number_analog])
             #         value = a * x + b
             #         self.matrix_analog[t, number_analog + 1] = value
-            self.matrix_digital = numpy.zeros((len(data), self.__numbers_digital + 1))
-            for t in range(len(data)):
-                self.matrix_digital[t, 0] = int(data[t].split(",")[1])
+            self.matrix_digital = numpy.zeros((len(self.data_dat), self.__numbers_digital + 1))
+            for t in range(len(self.data_dat)):
+                self.matrix_digital[t, 0] = int(self.data_dat[t].split(",")[1])
                 for number_digital in range(self.__numbers_digital):
                     self.matrix_digital[t, number_digital + 1] = int(
-                        data[t].split(",")[2 + self.__numbers_analog + number_digital])
+                        self.data_dat[t].split(",")[2 + self.__numbers_analog + number_digital])
         fp.close()
 
     def plotFig(self, data):
@@ -76,5 +84,3 @@ class Parser_comtrade():
         self.index_time = data_flag.index(1)
         # print("Time = ", self.matrix_digital[self.index_time, 0])
         return self.matrix_digital[self.index_time, 0]
-
-
--- main.py
+++ main.py
@@ -14,40 +14,87 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 import shutil
 from ParserComtrade import Parser_comtrade
+from functools import reduce
 
-parser_rtds_comtrade = Parser_comtrade(path="1", flag="start_test")
-parser_terminal_comtrade = Parser_comtrade(path="2", flag="224 Пуск осциллогр.")
-#
-# parser_rtds_comtrade.pars_cfg()
-# parser_rtds_comtrade.pars_dat()
-# t1 = parser_rtds_comtrade.search_start_time()
-# parser_terminal_comtrade.pars_cfg()
-# parser_terminal_comtrade.pars_dat()
-# t2 = parser_terminal_comtrade.search_start_time()
-# list_times_before = []
-# count_before = 0
-# time_for_before = t1
-# while time_for_before > 0 and len(list_times_before) < (parser_terminal_comtrade.index_time + 1):
-#     list_times_before.append(time_for_before)
-#     count = int(883 / 50) * 50
-#     time_for_before = time_for_before - count
-# print(len(list_times_before))
-# list_times_after = []
-# count_after = 0
-# time_for_after = t1
-# print(parser_terminal_comtrade.end_time)
-# while len(list_times_after) < (parser_terminal_comtrade.index_end_time - parser_terminal_comtrade.index_time - 1):
-#     count = int(883 / 50) * 50
-#     time_for_after = time_for_after + count
-#     list_times_after.append(time_for_after)
-# print(len(list_times_after))
-# times = (list_times_before + list_times_after)
-# times.sort()
-# index_times = list(map(lambda x: x / 50, times))
-# name_digital_rtds = parser_rtds_comtrade.name_digital
+parser_rtds_comtrade = Parser_comtrade(path="1", flag="start_test", A=False, D=True)
+parser_terminal_comtrade = Parser_comtrade(path="2", flag="224 Пуск осциллогр.", A=True, D=True)
 
+parser_rtds_comtrade.pars_cfg()
+parser_rtds_comtrade.pars_dat()
+t1 = parser_rtds_comtrade.search_start_time()
+parser_terminal_comtrade.pars_cfg()
+parser_terminal_comtrade.pars_dat()
+t2 = parser_terminal_comtrade.search_start_time()
+list_times_before = []
+count_before = 0
+time_for_before = t1
+while time_for_before > 0 and len(list_times_before) < (parser_terminal_comtrade.index_time + 1):
+    list_times_before.append(time_for_before)
+    count = int(883 / 50) * 50
+    time_for_before = time_for_before - count
+print(len(list_times_before))
+list_times_after = []
+count_after = 0
+time_for_after = t1
+print(parser_terminal_comtrade.end_time)
+while len(list_times_after) < (parser_terminal_comtrade.index_end_time - parser_terminal_comtrade.index_time - 1):
+    count = int(883 / 50) * 50
+    time_for_after = time_for_after + count
+    list_times_after.append(time_for_after)
+print(len(list_times_after))
+times = (list_times_before + list_times_after)
+times.sort()
+
+
+# index_times = list(map(lambda x: (int(x / 50)), times))
+
+
+def filter_max(x):
+    if x < 60000:
+        return True
+    return False
+
+
+def list_to_str(list_data):
+    string = ""
+    for i in range(len(list_data)):
+        string = string + ", " + str(int(list_data[i]))
+    return string
+
+index_times = list(filter(filter_max, list(map(lambda x: (int(x / 50)), times))))
+print(max(index_times))
 shutil.copyfile("2.cfg", "3.cfg")
 shutil.copyfile("2.dat", "3.dat")
-# for name_digital in range(len(name_digital_rtds)):
-#     for index in range(len(index_times)):
-#         print("hi")
+name_digital_rtds = parser_rtds_comtrade.name_digital
+with open("3.cfg", 'w') as fp:
+    number_str = 2 + parser_terminal_comtrade.numbers_signal
+    data_cfg_terminal = parser_terminal_comtrade.data_cfg
+    for new_index_digital in range(len(name_digital_rtds)):
+        new_str = str(len(parser_terminal_comtrade.name_digital) + new_index_digital + 1) + "," + name_digital_rtds[
+            new_index_digital] + ",0\n"
+        data_cfg_terminal.insert(number_str + new_index_digital, new_str)
+    new_number_digital = len(parser_terminal_comtrade.name_digital) + len(parser_rtds_comtrade.name_digital)
+    list_1 = data_cfg_terminal[1].split(",")
+    data_cfg_terminal[1] = str(len(parser_terminal_comtrade.name_analog) + new_number_digital) + "," + str(
+        len(parser_terminal_comtrade.name_analog)) + "A," + str(new_number_digital) + "D\n"
+    for i in range(len(data_cfg_terminal)):
+        fp.write(data_cfg_terminal[i])
+
+with open("3.dat", 'w') as fp:
+    data_dat_terminal = parser_terminal_comtrade.data_dat
+    # a = parser_terminal_comtrade.matrix_digital
+    # matrix_digital_rtds = parser_rtds_comtrade.matrix_digital
+    for index in range(len(index_times)):
+        list_data_digital = list(parser_rtds_comtrade.matrix_digital[index_times[index], 2:])
+        # str_data_digital = reduce(lambda x, y: str(x) + ", " + str(int(y)), list_data_digital)
+        str_data_digital = list_to_str(list_data_digital)
+        fp.write(data_dat_terminal[index].split("\n")[0] + ", " + str_data_digital + "\n")
+# def list_to_str(list_data):
+#     string=""
+#     for i in range(len(list_data)):
+#         string = string + ", "+ str(int(list_data[i]))#
+#     return string
+# a= [1,2,3]
+# print(list_to_str(a))
+# str_data_digital = reduce(lambda x, y: str((x)) + ", " + str((int(y))), a)
+# print(str_data_digital)
